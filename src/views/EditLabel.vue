00f2703e3487f7cec57c50c393da2ce9bf41a979
kiki <772765729@qq.com>
2020-02-07 22:35:46 +0800

    修改了editlabel的CSS

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -1,15 +1,20 @@
 <template>
   <div class="editLabel">
-    <router-link to="/labels" class="link">
-      <Icon name="left" />
-      <p>返回</p>
-    </router-link>
-    <div class="name">
-      <input v-model="tagName" placeholder="不超过4个字" required maxlength="4" />
+    <div class="title">
+      <router-link to="/labels" class="link">
+        <Icon name="left" />返回
+      </router-link>
+      <div class="titleText">编辑标签</div>
     </div>
-    <div class="buttons">
-      <Button text="确 认" type="primary" :handleClick="editLabel" />
-      <Button text="删除标签" type="normal" :handleClick="deleteLabel" />
+    <div class="line"></div>
+    <div class="main">
+      <div class="name">
+        <input v-model="tagName" placeholder="不超过4个字" required maxlength="4" />
+      </div>
+      <div class="buttons">
+        <Button text="确 认" type="primary" :handleClick="editLabel" />
+        <Button text="删除标签" type="normal" :handleClick="deleteLabel" />
+      </div>
     </div>
   </div>
 </template>
@@ -38,13 +43,13 @@ export default class EditLabel extends Vue {
     }
   }
   editLabel() {
-    if (
-      this.tagName &&
-      this.tagName !== "" &&
-      !this.tags.find(tag => tag.name === this.tagName)
-    ) {
-      this.tags[this.tagIndex].name = this.tagName;
-      this.end();
+    if (this.tagName && this.tagName !== "") {
+      if (this.tags.find(tag => tag.name === this.tagName)) {
+        window.alert("标签名重复");
+      } else {
+        this.tags[this.tagIndex].name = this.tagName;
+        this.end();
+      }
     }
   }
   deleteLabel() {
@@ -61,39 +66,58 @@ export default class EditLabel extends Vue {
 <style lang="scss" scoped>
 @import "~@/assets/style/normal.scss";
 .editLabel {
-  .link {
+  .title {
     margin: 0 20px;
     padding-top: 44px;
-    font-size: $font-size-m;
     height: 88px;
-    color: $black;
-    display: flex;
-    align-items: center;
-    text-align: center;
-    .icon {
-      margin-right: 4px;
-      height: 12px;
-    }
-  }
-
-  .name {
-    margin: 20px;
-    > input {
-      width: 100%;
-      height: 50px;
-      padding-left: 20px;
-      background-color: $light-grey;
-      border-radius: $border-radius-m;
-      border-style: none;
+    .link {
+      position: fixed;
+      top: 44px;
+      left: 20px;
+      height: 44px;
       font-size: $font-size-m;
-      color: $dark-grey;
+      color: $black;
+      display: flex;
+      align-items: center;
+      text-align: center;
+      .icon {
+        margin-right: 4px;
+        height: 12px;
+      }
+    }
+    .titleText {
+      font-size: $font-size-l;
+      height: 44px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
     }
   }
-  .buttons {
-    margin: 20px;
-    margin-top: 40px;
-    button {
-      margin-bottom: 10px;
+  .line {
+    width: 100vw;
+    height: 10px;
+    background-color: $light-grey;
+  }
+  .main {
+    .name {
+      margin: 20px;
+      > input {
+        width: 100%;
+        height: 50px;
+        padding-left: 20px;
+        background-color: $light-grey;
+        border-radius: $border-radius-m;
+        border-style: none;
+        font-size: $font-size-m;
+        color: $dark-grey;
+      }
+    }
+    .buttons {
+      margin: 20px;
+      margin-top: 40px;
+      button {
+        margin-bottom: 10px;
+      }
     }
   }
 }
