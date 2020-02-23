bee1a45e45524b47193fd73c9d7da70d0e26715b
kiki <772765729@qq.com>
2020-02-23 22:23:22 +0800

    重写tagstatistics页面组件，修改tag被删除后原先使用该tag的记录无法显示的问题

--- src/views/TagStatistics.vue
+++ src/views/TagStatistics.vue
@@ -1,8 +1,8 @@
 <template>
-  <HeaderLayout :titleText="tag.name">
+  <HeaderLayout :titleText="tagName">
     <div class="bills">
       <div class="yearAndMonth">{{yearAndMonth}}</div>
-      <BillItem v-for="bill in data" :key="bill.time" :bill="bill" />
+      <BillItem v-for="bill in bills" :key="bill.time" :bill="bill" />
       <div class="line"></div>
     </div>
   </HeaderLayout>
@@ -18,20 +18,22 @@ import { Component, Prop, Watch } from "vue-property-decorator";
   components: { BillItem }
 })
 export default class TagStatistics extends Vue {
-  tag: Tag | undefined;
-  tagName: string | undefined;
-  now: string | undefined;
-  data: Bill[] | undefined;
-  yearAndMonth: string | undefined;
+  get tagName() {
+    return this.$route.params.id.split("-")[0];
+  }
+  get date() {
+    return new Date(Number(this.$route.params.id.split("-")[1]));
+  }
+  get yearAndMonth() {
+    return dayjs(this.date).format("YYYY-M");
+  }
+  get bills() {
+    return (
+      this.$store.getters.getBillsByTag(this.date)[this.tagName].data || []
+    );
+  }
   created() {
-    this.tagName = this.$route.params.id.split("-")[0];
-    this.tag = this.$store.getters.findTag(this.tagName);
-    if (!this.tag) this.$router.replace("/404");
-    this.now = this.$route.params.id.split("-")[1];
-    const date = new Date(Number(this.now));
-    this.yearAndMonth = dayjs(date).format("YYYY-M");
-    this.data =
-      this.$store.getters.getBillsByTag(date)[this.tagName].data || [];
+    this.$store.commit("fetch");
   }
 }
 </script>
