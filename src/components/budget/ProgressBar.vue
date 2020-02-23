745ddb8ed9c49b4f39652b2b795dce3e91a27669
kiki <772765729@qq.com>
2020-02-23 19:01:41 +0800

    预算页面的组件化

---
+++ src/components/budget/ProgressBar.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="progressBar">
+    <div class="percentLine">
+      <div class="use" :style="`width: ${expend/budget*100}%;max-width:100%`"></div>
+      <div class="line" :style="`left: ${now.getDate()/endDate*100}%`"></div>
+    </div>
+    <div class="date">
+      <div class="startDate">{{now.getMonth()+1}}月1日</div>
+      <div class="endDate">{{endDate}}日</div>
+    </div>
+    <Icon name="scale" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Component, Prop, Watch } from "vue-property-decorator";
+@Component
+export default class ProgressBar extends Vue {
+  @Prop({ default: new Date() }) now!: Date;
+  @Prop({ default: 0 }) endDate!: number;
+  @Prop({ default: 0 }) expend!: number;
+  @Prop({ default: 0 }) budget!: number;
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~@/assets/style/normal.scss";
+.progressBar {
+  border-bottom: 1px solid $border-grey;
+  .icon {
+    width: 100%;
+    height: 10px;
+  }
+  .percentLine {
+    height: 20px;
+    margin: 0 20px;
+    background-color: $light-yellow;
+    position: relative;
+    border-radius: $border-radius-m;
+    .use {
+      border-radius: $border-radius-m;
+      height: 20px;
+      background-image: linear-gradient(141deg, #ff8700 0%, #ffce4c 100%);
+      position: absolute;
+      top: 0;
+      left: 0;
+    }
+    .line {
+      height: 20px;
+      width: 1px;
+      background-color: $orange;
+      position: absolute;
+      top: 0;
+      .day {
+        position: absolute;
+        top: 35px;
+        left: -20px;
+        width: 40px;
+        color: $orange;
+      }
+    }
+  }
+  .date {
+    display: flex;
+    justify-content: space-between;
+    margin: 5px 20px 2px 20px;
+    color: $grey;
+    position: relative;
+    .nowDate {
+      color: $orange;
+      position: absolute;
+    }
+  }
+}
+</style>
--- src/views/Budget.vue
+++ src/views/Budget.vue
@@ -1,43 +1,34 @@
 <template>
   <HeaderLayout titleText="本月预算">
-    <div class="top">
-      <div class="rest">
-        <div class="text">剩余预算</div>
-        <div class="count">{{rest}}</div>
-      </div>
-      <div class="percentLine">
-        <div class="use" :style="`width: ${expend/budget*100}%;max-width:100%`"></div>
-        <div class="line" :style="`left: ${now.getDate()/endDate*100}%`"></div>
-      </div>
-      <div class="date">
-        <div class="startDate">{{mm+1}}月1日</div>
-        <div class="endDate">{{endDate}}日</div>
-      </div>
-      <Icon name="scale" />
+    <div class="rest">
+      <div class="text">剩余预算</div>
+      <div class="count">{{rest}}</div>
     </div>
+    <ProgressBar :now="now" :expend="expend" :budget="budget" :endDate="endDate" />
     <Lists :lists="lists" />
-    <router-link to="/settingBudget" class="button">
-      <Button text="设置每月预算" type="primary" />
-    </router-link>
+    <div class="button">
+      <router-link to="/settingBudget">
+        <Button text="设置每月预算" type="primary" />
+      </router-link>
+    </div>
   </HeaderLayout>
 </template>
 
 <script lang="ts">
 import BillCommon from "@/mixins/BillCommon";
 import Lists from "@/components/budget/Lists.vue";
+import ProgressBar from "@/components/budget/ProgressBar.vue";
 import HeaderLayout from "@/components/layout/HeaderLayout.vue";
 import Vue from "vue";
 import { Component, Prop, Watch } from "vue-property-decorator";
 import { mixins } from "vue-class-component";
 
 @Component({
-  components: { Lists, HeaderLayout }
+  components: { Lists, HeaderLayout, ProgressBar }
 })
 export default class Budget extends mixins(BillCommon) {
   now = new Date();
-  yy = this.now.getFullYear();
-  mm = this.now.getMonth();
-  endDate = this.dayOfMonth(this.yy, this.mm);
+  endDate = this.dayOfMonth(this.now.getFullYear(), this.now.getMonth());
   monthStats = this.$store.getters.getMonthStats(this.now);
   get budget() {
     return this.$store.state.budget;
@@ -48,13 +39,12 @@ export default class Budget extends mixins(BillCommon) {
   get rest() {
     return this.budget - this.expend > 0 ? this.budget - this.expend : 0;
   }
-  get restDay() {
-    return this.endDate - this.now.getDate();
-  }
   get lists() {
     const dailyCanUse =
       this.budget - this.expend > 0
-        ? this.fixTwo((this.budget - this.expend) / this.restDay)
+        ? this.fixTwo(
+            (this.budget - this.expend) / (this.endDate - this.now.getDate())
+          )
         : 0;
     return [
       { text: "总预算", count: this.budget },
@@ -70,17 +60,6 @@ export default class Budget extends mixins(BillCommon) {
 
 <style lang="scss" scoped>
 @import "~@/assets/style/normal.scss";
-.top {
-  background-color: white;
-  box-shadow: 0 2px 14px 0 rgba(0, 0, 0, 0.05);
-  .icon {
-    width: 100%;
-    height: 10px;
-  }
-}
-.button {
-  margin: 20px;
-}
 .rest {
   display: flex;
   flex-direction: column;
@@ -96,45 +75,7 @@ export default class Budget extends mixins(BillCommon) {
     font-family: $font-number;
   }
 }
-.percentLine {
-  height: 20px;
-  margin: 0 20px;
-  background-color: $light-yellow;
-  position: relative;
-  border-radius: $border-radius-m;
-  .use {
-    border-radius: $border-radius-m;
-    height: 20px;
-    background-image: linear-gradient(141deg, #ff8700 0%, #ffce4c 100%);
-    position: absolute;
-    top: 0;
-    left: 0;
-  }
-  .line {
-    height: 20px;
-    width: 1px;
-    background-color: $orange;
-    position: absolute;
-    top: 0;
-    .day {
-      position: absolute;
-      top: 35px;
-      left: -20px;
-      width: 40px;
-      color: $orange;
-    }
-  }
-}
-
-.date {
-  display: flex;
-  justify-content: space-between;
-  margin: 5px 20px 0 20px;
-  color: $grey;
-  position: relative;
-  .nowDate {
-    color: $orange;
-    position: absolute;
-  }
+.button {
+  margin: 20px;
 }
 </style>
